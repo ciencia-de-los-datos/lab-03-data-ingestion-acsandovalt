6bfe58e69ff242096df642e7887579610b9085b2
acsandovalt <acsandovalt@unal.edu.co>
2024-03-31 12:11:38 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -9,13 +9,55 @@ espacio entre palabra y palabra.
 
 
 """
+
 import pandas as pd
 
+# Variable global para almacenar el DataFrame
+dataframe = None
 
 def ingest_data():
+    global dataframe
+    
+    # Si no, procedemos a generar el DataFrame
+    data = []
 
-    #
-    # Inserte su código aquí
-    #
+    with open("clusters_report.txt", "r") as file:
+        for _ in range(3):
+            next(file)
 
-    return df
+        for line in file:
+            parts = line.split(maxsplit=3)
+            if len(parts) < 4:
+                continue
+            try:
+                cluster = int(parts[0])
+                cantidad = int(parts[1])
+                porcentaje = float(parts[2].replace(',', '.'))
+            except ValueError:
+                continue
+            palabras_clave = parts[3].strip().replace('  ', '').replace('%', '')
+
+            palabras_clave = palabras_clave.lstrip().rstrip()
+            
+            parts[3:3] = parts[2].strip().replace(',', '')
+            data.append([cluster, cantidad, porcentaje, palabras_clave])
+
+    dataframe = pd.DataFrame(data, columns=["cluster", "cantidad_de_palabras_clave", "porcentaje_de_palabras_clave", "principales_palabras_clave"])
+    dataframe.columns = dataframe.columns.str.lower().str.replace(' ', '_')
+
+    return dataframe
+
+ingest_data()
+
+#print(dataframe)
+
+
+
+
+
+
+
+
+
+
+
